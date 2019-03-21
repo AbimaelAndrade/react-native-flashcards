f11c878e29687521c001b903bacffac7ce5009dd
abimaelandrade <abimaelc.andrade@gmail.com>
2019-03-20 23:32:34 -0300

    set notification

--- App.js
+++ App.js
@@ -4,10 +4,15 @@ import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middlewares";
 import { StyleSheet, View, Text } from "react-native";
+import { setLocalNotification } from "./utils/notification";
 
 const store = createStore(reducer, middleware);
 
 export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+
   render() {
     return (
       <Provider store={store}>
